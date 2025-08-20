c0bc03a47067323dc384f0b0a17189bcd898a656
404error-gs <gensi1955@gmail.com>
2025-08-20 11:30:48 -0500

    Variable Excersice

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Genesis";
+            int myAge = 18;
+            char myInitial = 'G';
+            bool isStudent = true;
+            double myHeight = 5.4;
+            decimal myWeight = 125.5m;
+
+
+            Console.WriteLine($"Name: {myName}, I am {myAge} years old, and {myHeight} feet tall");
         }
     }
 }
